80b99914583d597b445f14988c57d24ea0514385
Maykel Arias Torres <dev0006@simelo.tech>
2019-11-27 10:32:16 -0500

    [fibercrypto] refs #1 Init module

--- fibercryptopy/__init__.py
+++ fibercryptopy/__init__.py
@@ -1,1 +1,16 @@
 __version__ = "0.27.0.dev0"
+
+init_error = None
+
+def _print2stderr(msg):
+    sys.stderr.write(msg + '\n')
+
+try:
+    from .fibercryptopy import *
+except (AttributeError, ImportError) as _err :
+    init_error = _err
+
+    import sys, traceback
+    _print2stderr("Error initializing fibercryptopy package. Details:")
+    traceback.print_exception(*sys.exc_info())
+    _print2stderr("\n\nInport succeeded but package load failed")
